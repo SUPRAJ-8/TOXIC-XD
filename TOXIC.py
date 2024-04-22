6ad2b0f126446732883361d5785d7adc02ae6f64
SUPRAJ-8 <129602272+SUPRAJ-8@users.noreply.github.com>
2024-04-22 13:10:32 +0545

    Update TOXIC.py

--- TOXIC.py
+++ TOXIC.py
@@ -579,31 +579,37 @@ def menu():
 #     # Define your setting function here
 #     pass
 
+import os
 import time
 
-def back():
-    pass  # Placeholder for the back function
-
-def info():
-    pass  # Placeholder for the info function
-
 def crack_file():
-    os.system('clear')
-    banner()
-    info()
-    print(' [\u001b[36m•\033[1;37m] Input File Name Without /sdcard ')
-    linex()
-    o1 = input(' [\u001b[36m•\033[1;37m] Enter The Name Of File : ')
-    o = '/sdcard/'+o1
-    try:
-        lin = open(o).read().splitlines()
-    except:
-        linex()
-        animation(' [×] File As You Enter Does Not Exits ')
-        time.sleep(2)
-        back()
+    while True:
+        print('\033[0;97m-----------------------------------------------')
+        filename = input(' [\u001b[36m•\033[1;37m] Enter The Name Of File : ')
+        o = '/sdcard/'+filename  # Form the complete file path
+        try:
+            with open(o) as file:
+                lines = file.read().splitlines()
+            break  # Exit the loop if the file is successfully opened
+        except FileNotFoundError:
+            print('\033[0;97m-----------------------------------------------')
+            print(' [×] FILE NOT FOUND')
+            input(" Please Press Enter to retry...")
+    
+    # Assuming `id` is defined elsewhere in your code
+    id = []
+    for line in lines:
+        id.append(line)
+    
+    # Call the setting function
+    setting()
+
+def setting():
+    # Define your setting function here
+    pass
 
 crack_file()
+
 
 #-------------[ PENGATURAN-IDZ ]---------------#
  
