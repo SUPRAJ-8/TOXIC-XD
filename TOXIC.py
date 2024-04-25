b2260a310b3fb9658bdd9e2cae777f610136dcae
SUPRAJ-8 <129602272+SUPRAJ-8@users.noreply.github.com>
2024-04-25 11:29:07 +0545

    Update TOXIC.py

--- TOXIC.py
+++ TOXIC.py
@@ -559,12 +559,10 @@ import time
 def crackfile():
     while True:
         print('\033[0;97m-----------------------------------------------')
+        print(' [\u001b[36m•\033[1;37m] Input File Name Without /sdcard ')
         filename = input(' [\u001b[36m•\033[1;37m] Enter The Name Of File : ')
         o = '/sdcard/'+filename  # Form the complete file path
-        try:
-            with open(filename) as file:
-                lines = open(o).read().splitlines()
-            break  # Exit the loop if the file is successfully opened
+        try:lines = open(o).read().splitlines()
         except FileNotFoundError:
             print('\033[0;97m-----------------------------------------------')
             print(' [×] FILE NOT FOUND')
