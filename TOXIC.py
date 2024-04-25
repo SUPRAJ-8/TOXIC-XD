92e5269face897ae7f1c1fea0d46d72fb43d58dc
SUPRAJ-8 <129602272+SUPRAJ-8@users.noreply.github.com>
2024-04-25 12:20:46 +0545

    Update TOXIC.py

--- TOXIC.py
+++ TOXIC.py
@@ -558,8 +558,9 @@ import time
 def crackfile():
     while True:
         print('\033[0;97m-----------------------------------------------')
+        print(' [\u001b[36m•\033[1;37m] Input File Name Without /sdcard/ ')
         filename = input(' [\u001b[36m•\033[1;37m] Enter The Name Of File : ')
-        o = '/sdcard/'+filename  # Form the complete file path
+        o = '/sdcard/'+filename # Form the complete file path
         try:
             with open(o) as file:
                 lines = file.read().splitlines()
@@ -574,11 +575,6 @@ def crackfile():
         id.append(line)
 
         setting()
-
-# Call the crackfile function
-
-
-
 
 
 #-------------[ PENGATURAN-IDZ ]---------------#
