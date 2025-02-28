b0ca519b752f6b6ce5a7b5a3320377932e367f38
Supraj-XD <129602272+SUPRAJ-8@users.noreply.github.com>
2025-02-28 09:55:20 +0545

    Update TOXIC.py

--- TOXIC.py
+++ TOXIC.py
@@ -444,7 +444,7 @@ def linex():
     print('\033[1;37m')
 def animation(u):
     for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.01)
-logo ="""
+logo =r"""
  \033[1;91m_____ _____  _____ ____ 
  \033[1;92m|_   _/ _ \ \/ |_ _/ ___|
  \033[1;93m  | || | | \  / | | |    
